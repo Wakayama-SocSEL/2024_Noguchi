8c25db2092081277f7cd214526aeee6daafa9d7d
tomoya-n <s266227@wakayama-u.ac.jp>
2023-11-23 05:51:23 +0900

    ディレクトリ構成変更に伴うPATHの変更

--- app/clean.py
+++ app/clean.py
@@ -2,14 +2,15 @@ import re
 import os
 
 # コメント、空白行を削除し、[MESSAGES CONTROL]のみを出力
-PATH = '/Users/tomoya-n/dev/pylintrc/gunicorn.txt'
-PATH_OUTPUT = '/Users/tomoya-n/dev/output/output.txt'
+PATH = '/Users/tomoya-n/dev/2024_Noguchi/data/'
+PATH_IN = PATH + 'pylintrc/gunicorn.txt'
+PATH_OUTPUT = PATH + '/output/output.txt'
 char_remove = '#'
 title = '[MESSAGES CONTROL]'
 
 with open(PATH_OUTPUT, 'w') as f:
     in_messages_control = False
-    for line in open(PATH, 'r'):
+    for line in open(PATH_IN, 'r'):
         if char_remove in line:
             continue
         if not line.strip():
