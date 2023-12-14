3c7ba7c5c495db203f9f309e33aed4537ff8894b
tomoya-n <s266227@wakayama-u.ac.jp>
2023-12-15 00:51:51 +0900

    実際にある規約数より少ない問題の解決

--- app/utils/get_file_name.py
+++ app/utils/get_file_name.py
@@ -2,9 +2,9 @@ import os
 
 def get_file_name(directory):
     FILE_NAME_LIST = []
-    for filename in os.listdir(directory):
+    for filename in sorted(os.listdir(directory), key=lambda x: x.lower()):
         if filename.endswith(".txt"):
             file_name = os.path.splitext(filename)[0]
             FILE_NAME_LIST.append(file_name)
-    
+
     return FILE_NAME_LIST
